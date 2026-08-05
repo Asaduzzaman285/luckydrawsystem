c698df6d3e82add26e244d9f72f26a94fedf1908
Asaduzzaman285 <asaduzzaman.14333@gmail.com>
2026-08-06 00:23:39 +0600

    Force HTTPS scheme in production for Cloudflare Flexible SSL

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,5 +22,9 @@ class AppServiceProvider extends ServiceProvider
         \Illuminate\Support\Facades\Gate::before(function ($user, $ability) {
             return $user->hasRole('super-admin') ? true : null;
         });
+
+        if (env('APP_ENV') !== 'local') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
